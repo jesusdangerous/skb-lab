b90a7cc5a32b75235bf3395f9b5a40c8453003d0
jesusdangerous <aleksandrzonov641@gmail.com>
2025-02-21 13:01:29 +0500

    initial commit

---
+++ skb-lab/src/main/java/ru/zonov/skb_lab/SkbLabApplication.java
@@ -0,0 +1,13 @@
+package ru.zonov.skb_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkbLabApplication.class, args);
+	}
+
+}
---
+++ skb-lab/src/test/java/ru/zonov/skb_lab/SkbLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zonov.skb_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
